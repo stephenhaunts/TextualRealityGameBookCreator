2fc50bbe9eb03064b1cec2dbc2efcf8f84a57235
Stephen Haunts <stephen.haunts@outlook.com>
2019-05-06 13:29:25 +0100

    Add section classes for a paragraph and image.

---
+++ TextualRealityGameBookCreator.Tests.Unit/Section Primitives/ImageTests.cs
@@ -0,0 +1,65 @@
+/*
+MIT License
+
+Copyright (c) 2019 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextualRealityGameBookCreator.SectionPrimitives;
+
+namespace TextualRealityGameBookCreator.Tests.Unit.SectionPrimitives
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void DefaultConstructorDefeultsFileNameToEmptyString()
+        {
+            Image image = new Image();
+
+            Assert.IsNotNull(image.FileName);
+        }
+
+        [TestMethod]
+        public void OverrideConstructorAssignsSpecifiedFileNAme()
+        {
+            const string FileName = "./image.png";
+            Image image = new Image(FileName);
+
+            Assert.IsNotNull(image.FileName);
+            Assert.AreEqual(FileName, image.FileName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OverrideConstructorThrowsArgumentNullExceptionIfSpecifiedFileNameIsNull()
+        {
+            Image image = new Image(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OverrideConstructorThrowsArgumentNullExceptionIfSpecifiedFileNameIsEmptyString()
+        {
+            Image image = new Image(string.Empty);
+        }
+    }
+}
---
+++ TextualRealityGameBookCreator.Tests.Unit/Section Primitives/ParagraphTests.cs
@@ -0,0 +1,65 @@
+/*
+MIT License
+
+Copyright (c) 2019 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextualRealityGameBookCreator.SectionPrimitives;
+
+namespace TextualRealityGameBookCreator.Tests.Unit.SectionPrimitives
+{
+    [TestClass]
+    public class ParagraphTests
+    {
+        [TestMethod]
+        public void DefaultConstructorDefeultsTextToEmptyString()
+        {
+            Paragraph paragraph = new Paragraph();
+
+            Assert.IsNotNull(paragraph.Text);
+        }
+
+        [TestMethod]
+        public void OverrideConstructorAssignsSpecifiedText()
+        {
+            const string Text = "This is my paragraph text.";
+            Paragraph paragraph = new Paragraph(Text);
+
+            Assert.IsNotNull(paragraph.Text);
+            Assert.AreEqual(Text, paragraph.Text);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OverrideConstructorThrowsArgumentNullExceptionIfSpecifiedTextIsNull()
+        {
+            Paragraph paragraph = new Paragraph(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OverrideConstructorThrowsArgumentNullExceptionIfSpecifiedTextIsEmptyString()
+        {
+            Paragraph paragraph = new Paragraph(string.Empty);
+        }
+    }
+}
